c9f28e68a1bab6a17403ea1111b908f367cfeffd
Kordian Rybarczyk <250993@edu.p.lodz.pl>
2024-04-05 16:18:29 +0200

    Dzielenie

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ void usage(char *programName){
     printf("Usage: %s number1 operation number2\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
+    printf("\tdiv\n");
     printf("\tmul\n");
     printf("\tsub\n");
     printf("\n");
@@ -23,6 +24,21 @@ int main(int argc, char* argv[]) {
         if (!strcmp("add", argv[2])) {
             result = number1 + number2;
             printf("%d + %d = %d\n", number1, number2, result);
+            return 0;
+        }
+        if (!strcmp("div", argv[2])) {
+            if (number2!= 0) {
+                result = number1 / number2;
+                printf("%d / %d = %d\n", number1, number2, result);
+            } else {
+                if (number1 == 0) {
+                    printf("%d / %d = NaN\n", number1, number2);
+                } else if (number1 > 0) {
+                    printf("%d / %d = +INF\n", number1, number2);
+                } else {
+                    printf("%d / %d = -INF\n", number1, number2);
+                }
+            }
             return 0;
         }
         if (!strcmp("mul", argv[2])) {

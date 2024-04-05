1db66b6d8e25fa8429ba5773b42780898d75babf
Pawel Piasecki <250988@edu.p.lodz.pl>
2024-04-05 16:22:22 +0200

    adding multiplication

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ void usage(char *programName){
     printf("Usage: %s number1 operation number2\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
+    printf("\tmul\n");
     printf("\tsub\n");
     printf("\n");
 }
@@ -22,6 +23,11 @@ int main(int argc, char* argv[]) {
         if (!strcmp("add", argv[2])) {
             result = number1 + number2;
             printf("%d + %d = %d\n", number1, number2, result);
+            return 0;
+        }
+        if (!strcmp("mul", argv[2])) {
+            result = number1 * number2;
+            printf("%d * %d = %d\n", number1, number2, result);
             return 0;
         }
 

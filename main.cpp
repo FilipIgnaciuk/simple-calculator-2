da920303f3bab81050c8fa41570846fd372175bb
Filip Ignaciuk <@50965@edu.p.lodz.pl>
2024-04-05 16:03:54 +0200

    adding

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <cstring>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void usage(char *programName){
+    printf("Usage: %s number1 operation number2\n", programName);
+    printf("Available operations:\n");
+    printf("\tadd\n");
+    printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+
+    int number1 = 0;
+    int number2 = 0;
+    int result = 0;
+
+    if (argc == 4) {
+        number1 = atoi(argv[1]);
+        number2 = atoi(argv[3]);
+
+        if (!strcmp("add", argv[2])) {
+            result = number1 + number2;
+            printf("%d + %d = %d\n", number1, number2, result);
+            return 0;
+        }
+    }
 }
